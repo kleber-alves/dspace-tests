28be37b8984b4920e75c8c55025b8917dc9fa4b0
Kleber Alves <kleber@pop-os.localdomain>
2022-03-21 22:43:34 -0300

    Action para conexão SSH em servidor.

--- docker/dspace/build-dspace.sh
+++ docker/dspace/build-dspace.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker rm -f local_bd_miguilim local/dspace_miguilim || true
-docker rmi -f $(docker images --filter=reference='local/:*' --format "{{.ID}}") || true
-docker-compose -f ./docker-compose.yml up -d
+docker rm -f local/dspace_miguilim || true
+# docker rmi -f $(docker images --filter=reference='local/:*' --format "{{.ID}}") || true
+docker-compose -f ./docker-compose.yml up -d dspace
