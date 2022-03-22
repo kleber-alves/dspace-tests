e6fc7f532fd5f202fb87d0dcf3e53544392696fe
Kleber Alves <kleber@pop-os.localdomain>
2022-03-21 22:31:24 -0300

    Action para conexão SSH em servidor.

--- docker/dspace/build-dspace.sh
+++ docker/dspace/build-dspace.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-docker rm -f dspace
-docker rmi -f $(docker images --filter=reference='mrgurgel/dspace:*' --format "{{.ID}}")
-docker build --build-arg PROJECT_NAME=dspace6-treinamento  -t mrgurgel/dspace /Users/mrgurgel/git/docker-dspace/dspace/
-docker run -d --name dspace --network nw-dspace mrgurgel/dspace
-
+docker rm -f local_bd_miguilim local/dspace_miguilim || true
+docker rmi -f $(docker images --filter=reference='local/:*' --format "{{.ID}}") || true
+docker-compose -f ./docker-compose.yml up -d
